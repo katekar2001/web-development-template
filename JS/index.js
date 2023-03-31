d845a35e88adb1460ffa61b685995e797eeaf923
aniket-katekar <aniketkatekar46916@gmai.com>
2023-03-31 16:43:36 +0530

    project images added

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,11 @@
+function sendForm(){
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    alert("Message sent successfully.");
+    alert(`Name : ${name} Email : ${email} Message : ${message}`);
+}
+
 $(document).ready(()=>{
     $('.navbar-toggler').click(()=>{ 
         $('.collapse').toggle(500);
